30b3f10247c0c80696fb048289462bd6b4f220c8
riyo3350g <yechouben@gmail.com>
2023-08-09 05:34:05 -0700

    add show method to the console

--- console.py
+++ console.py
@@ -5,6 +5,8 @@
 import cmd
 import json
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -32,6 +34,29 @@ class HBNBCommand(cmd.Cmd):
         new_inst = BaseModel()
         new_inst.save()
         print(new_inst.id)
+
+    def do_show(self, line):
+        """
+        Prints the string representation of an
+        instance based on class name & id
+        """
+        arguments = line.split()
+        if not arguments:
+            print('** class name missing **')
+            return
+        if arguments[0] not in self.own_classes:
+            print('** class doesn\'t exist **')
+            return
+        if len(arguments) < 2:
+            print('** instance id missing **')
+            return
+
+        key = arguments[0] + '.' + arguments[1]
+        all_objcts = storage.all()
+        if key in all_objcts:
+            print(all_objcts[key])
+        else:
+            print('** no instance found **')
 
 
 if __name__ == '__main__':
