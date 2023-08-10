67a87660388ffe9f3dc3d95f0f8465145bab55fd
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-08-10 11:43:28 +0100

    testing: (state_model && city_model)

--- tests/test_models/test_state_model.py
+++ tests/test_models/test_state_model.py
@@ -17,3 +17,18 @@ class TestStateModel(unittest.TestCase):
         ''' testing State class that inherits from BaseModel '''
         first_state = State()
         self.assertIsInstance(first_state, BaseModel)
+
+    def test_state_attributes(self):
+        ''' testing state class attributes '''
+        att = ["name"]
+        first_state = State()
+        for idx in range(0, len(att)):
+            self.assertTrue(att[idx] in first_state.__dir__())
+
+    def test_state_name_type(self):
+        ''' test the type of the name '''
+        first_state = State()
+        # get the name value from the first_state object
+        name = getattr(first_state, "name")
+        # checks the type of the name value
+        self.assertIsInstance(name, str)
