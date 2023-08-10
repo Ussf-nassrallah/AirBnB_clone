677d83ad6f4cf26633023910eaa83f1df0f793da
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-08-10 11:55:12 +0100

    testing: (amenity_model && review_model)

--- tests/test_models/test_amenity_model.py
+++ tests/test_models/test_amenity_model.py
@@ -17,3 +17,18 @@ class TestAmenityModel(unittest.TestCase):
         ''' testing Amenity class that inherits from BaseModel '''
         first_amenity = Amenity()
         self.assertIsInstance(first_amenity, BaseModel)
+
+    def test_amenity_attributes(self):
+        ''' testing amenity class attributes '''
+        att = ["name"]
+        first_amenity = Amenity()
+        for idx in range(0, len(att)):
+            self.assertTrue(att[idx] in first_amenity.__dir__())
+
+    def test_amenity_name_type(self):
+        ''' test the type of the name '''
+        first_amenity = Amenity()
+        # get the name value from the first_Amenity object
+        name = getattr(first_amenity, "name")
+        # checks the type of the name value
+        self.assertIsInstance(name, str)
--- tests/test_models/test_review_model.py
+++ tests/test_models/test_review_model.py
@@ -17,3 +17,34 @@ class TestReviewModel(unittest.TestCase):
         ''' testing Review class that inherits from BaseModel '''
         first_review = Review()
         self.assertIsInstance(first_review, BaseModel)
+
+    def test_review_attributes(self):
+        ''' testing review class attributes '''
+        att = ["place_id", "user_id", "text"]
+        first_review = Review()
+        for idx in range(0, len(att)):
+            self.assertTrue(att[idx] in first_review.__dir__())
+
+    def test_review_place_id_type(self):
+        ''' test the type of the place_id '''
+        first_review = Review()
+        # get the place_id value from the first_review object
+        place_id = getattr(first_review, "place_id")
+        # checks the type of the place_id value
+        self.assertIsInstance(place_id, str)
+
+    def test_review_user_id_type(self):
+        ''' test the type of the user_id '''
+        first_review = Review()
+        # get the user_id value from the first_review object
+        user_id = getattr(first_review, "user_id")
+        # checks the type of the user_id value
+        self.assertIsInstance(user_id, str)
+
+    def test_review_text_type(self):
+        ''' test the type of the text '''
+        first_review = Review()
+        # get the text value from the first_review object
+        text = getattr(first_review, "text")
+        # checks the type of the text value
+        self.assertIsInstance(text, str)
