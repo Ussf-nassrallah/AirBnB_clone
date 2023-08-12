57e39fcd6db16a16342f91c874fa4e62664b8171
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-08-12 15:07:50 +0100

    Update console.py

--- console.py
+++ console.py
@@ -7,6 +7,15 @@ import json
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
+from shlex import split
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
